a397318b3da29bcb57b4aefd77b8223b6e220809
xonx <119700621+xonx4l@users.noreply.github.com>
2024-02-10 16:46:08 +0530

    Update publisher.go

--- pkg/pubs/publisher.go
+++ pkg/pubs/publisher.go
@@ -1,1 +1,8 @@
-
+// publish publishes a message to the given code 
+func (p *Publisher) publish(topic string, message []byte) error {
+  -, err := p.client.Publish(p.ctx, &pb.PublishRequeest{
+    Topic: topic,
+    Message: ,message
+    )}
+    return err
+  }
