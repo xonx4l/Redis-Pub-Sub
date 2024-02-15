3040837b24e1d78bed6a1045967a74f752da2921
xonx <119700621+xonx4l@users.noreply.github.com>
2024-02-15 23:22:12 +0530

    Update broker.go

--- pkg/pubs/broker.go
+++ pkg/pubs/broker.go
@@ -16,3 +16,15 @@ type Broker struct {
 	ctx                          context.context
 	cancel                       context.cancelfunc
 }
+func (b *Broker) Publish(ctx context.Context, in *pb.PublishRequest) (*pb.PublishResponse, error) {
+   b.mu.RLock()
+
+   brokerSubscribers := make([]streamKey, 0)
+   for subscriberId, stream := range b.subscribers[in.GetTopic()] {
+     key := streamKey{topic: in.GetTopic(), subscriberId: subscriberId}
+     b.topicSubscriberStreamMutexes[key].Lock()
+
+     err := stream.Send(&pb.Message{Topic: in.GetTopic(), Message: in.GetMessage()})
+     b.topicSubscriberStreamMutexes[key].Unlock()
+
+	   
